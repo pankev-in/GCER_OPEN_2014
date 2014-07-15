b0190394eb507fab80711ea5b2ebde4fc3ba0d49
Kevin Pan <pankevin128@gmail.com>
2014-07-15 15:03:49 +0200

    Main arm working

--- Software/DoubleAL/src/sketch/sketch.ino
+++ Software/DoubleAL/src/sketch/sketch.ino
@@ -1,4 +1,5 @@
 #include <Roomba.h>
+#include <Servo.h>
 
 // Defines the Roomba instance and the HardwareSerial it connected to:
 // &Serial1 (Arduino Mega only), Default Bandrate: 57600
@@ -7,6 +8,8 @@ Roomba roomba(&Serial1);
 
 //SensorPins:
 int LightSensorPin=0;
+Servo Servo1;
+Servo Servo2;
 
 //constance:
 int lightValue=650;
@@ -14,12 +17,16 @@ int lightValue=650;
 //variables
 int light=0;
 int distance=0;
+int Servo1_StartValue=100;
+int Servo2_StartValue=110;
 unsigned int u;
 int x;
 
 void setup()
 {
   Serial.begin(57600);          // baud rate of serial monitor
+  Servo1.attach(6);
+  Servo2.attach(7);
   roomba.start();
   roomba.fullMode();
   //roomba.safeMode();
@@ -37,6 +44,7 @@ void loop()
   roomba.driveDirect(-100,-100);
   delay(2000);
   roomba.driveDirect(0,0);
+  MainArm_Startposition();
   Serial.println("Position adjested.");
   Serial.println("Waiting for the light Signal...");
   while(light<=lightValue){
@@ -114,5 +122,18 @@ void turnRight(){
     roomba.driveDirect(0,0);
 }
 
+void MainArm_RightUp(){
+    Servo1.write(Servo1_StartValue-45);
+    Servo2.write(Servo2_StartValue+45);
+}
 
+void MainArm_LeftUp(){
+    Servo1.write(Servo1_StartValue+45);
+    Servo2.write(Servo2_StartValue-45);
+}
+
+void MainArm_Startposition(){
+    Servo1.write(Servo1_StartValue);
+    Servo2.write(Servo2_StartValue);
+}
 

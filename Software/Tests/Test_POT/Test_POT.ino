b26b09593de839ece66b2c84eb1e0453fb50abda
Kevin Pan <pankevin128@gmail.com>
2014-07-18 18:43:12 +0200

    test_pot

--- Software/Tests/Test_POT/Test_POT.ino
+++ Software/Tests/Test_POT/Test_POT.ino
@@ -1,4 +1,4 @@
-#define NUM_POTENTIOMETER_1_ZERO_DEGREE 555
+#define NUM_POTENTIOMETER_1_ZERO_DEGREE 500
 #define NUM_POTENTIOMETER_2_ZERO_DEGREE 450
 #define POTENTIOMETER_1_PIN  1
 #define POTENTIOMETER_2_PIN  2
@@ -12,6 +12,7 @@ void loop(){
   int j=CheckSecondaryArmAngle();
   int v1=analogRead(POTENTIOMETER_2_PIN);
   int v2=analogRead(POTENTIOMETER_1_PIN);  
+  
   Serial.print("Grab:");
   Serial.print(v1);
   Serial.print("/");
@@ -21,18 +22,18 @@ void loop(){
   Serial.print(v2);
   Serial.print("/");
   Serial.println(j);
-  */
   delay(500);
+  */
 }
 
 int CheckGrabAngle(){
   int value=analogRead(POTENTIOMETER_2_PIN);
-  value=map((value+511-NUM_POTENTIOMETER_2_ZERO_DEGREE),0,1023,-110,100);
+  value=int((value-NUM_POTENTIOMETER_2_ZERO_DEGREE)/(1023/230));
   return value;
 }
 
 int CheckSecondaryArmAngle(){
   int value=analogRead(POTENTIOMETER_1_PIN);
-  value=map((value+511-NUM_POTENTIOMETER_2_ZERO_DEGREE),0,1023,-110,100);
+  value=int((value-NUM_POTENTIOMETER_1_ZERO_DEGREE)/(1023/230));
   return value;
 }
